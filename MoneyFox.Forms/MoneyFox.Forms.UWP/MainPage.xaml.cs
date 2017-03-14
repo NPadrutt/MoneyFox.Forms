1b03ff5ab7f182c6c0ecd34808ebfcfa1fe438cd
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2017-03-14 22:24:00 +0100

    init

---
+++ MoneyFox.Forms/MoneyFox.Forms.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MoneyFox.Forms.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new MoneyFox.Forms.App());
+        }
+    }
+}
---
+++ MoneyFox.Forms/MoneyFox.Forms/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace MoneyFox.Forms.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
